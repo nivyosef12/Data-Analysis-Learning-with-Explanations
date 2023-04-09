ff6a04f7a03ca1ab0821b4cf0bddfcb2525d1508
nivyosef12 <nivyosef12@gmail.com>
2023-04-09 11:36:38 +0300

    implemented learning algorithm

--- feedbackModel.py
+++ feedbackModel.py
@@ -1,7 +1,11 @@
 # TODO
 # 1. what the hell is C[X] !?!?!?!?!
+# 2. why the fuck is the teacher returns the true label ?!?!?!?!?
+# 3. handle code duplication
 
 import numpy as np
+
+from Law import Law
 
 
 class feedbackModel:
@@ -21,11 +25,11 @@ class feedbackModel:
 
     def learn(self, X, y):
 
-        for feature, label in np.ndier([X, y]):
+        for features, label in np.ndier([X, y]):
             found = False
 
             for law in self.laws:
-                if law.equal(feature):
+                if law.isFitting(features):
                     found = True
                     predication = law.getLabel()
                     explanation = law.getExplanation()
@@ -34,8 +38,11 @@ class feedbackModel:
 
                     if predication != label:
                         # get discriminative feature from teacher
+                        discriminative_feature = self.teacher(features, label, explanation, predication)
+
                         # update the law
-                        # anything else ???
+                        law.updateFeatures(discriminative_feature)
+                        # TODO anything else??? , should break???
                         pass
 
             if not found:
@@ -46,7 +53,30 @@ class feedbackModel:
 
                 if predication != label:
                     # get discriminative feature from teacher
-                    # new_law = Law(discriminative feature, label)
-                    # self.L.add(new_law)
-                    # anything else ???
+                    discriminative_feature = self.teacher(features, label, explanation, predication)
+
+                    # update the laws with the new law
+                    new_law = Law(explanation, label, discriminative_feature)
+                    self.laws.append(new_law)
                     pass
+
+
+"""
+
+for i in range(10):
+    for j in range(5):
+        if j == 2:
+            # break out of the inner loop if j == 2
+            break
+        # if j != 2, continue with the rest of the inner loop
+        print(f"i={i}, j={j}")
+    else:
+        # this block of code is executed if the inner loop completes without hitting a break statement
+        print("inner loop completed")
+    # continue with the outer loop after the inner loop completes
+    continue
+    # these lines of code are not executed if j == 2
+    print("after inner loop")
+
+
+"""
