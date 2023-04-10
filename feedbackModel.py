7cdd1abcdec5aa925702a081333b9056775c5bcb
nivyosef12 <nivyosef12@gmail.com>
2023-04-10 19:26:21 +0300

    got rid of code duplication

--- feedbackModel.py
+++ feedbackModel.py
@@ -26,6 +26,56 @@ class feedbackModel:
     def learn(self, X, y):
 
         for features, label in np.ndier([X, y]):
+            predication, explanation, law = self.predict(features)
+
+            if predication != label:
+                # get discriminative feature from teacher
+                discriminative_feature = self.teacher(features, label, explanation, predication)
+
+                if law is None:
+                    # update the laws with the new law
+                    new_law = Law(explanation, label, discriminative_feature)
+                    self.laws.append(new_law)
+
+                else:
+                    # update the law
+                    law.updateFeatures(discriminative_feature)
+
+    def predict(self, features):
+
+        for law in self.laws:
+            if law.isFitting(features):
+
+                predication = law.getLabel()
+                explanation = law.getExplanation()
+
+                print(f"predicted: {predication}, with the explanation of {explanation}")
+                return predication, explanation, law
+
+        print(f"DEFAULT: predicted: {self.default_label}, with the explanation of {self.default_explanation}")
+        return self.default_label, self.default_explanation, None
+
+
+"""
+
+for i in range(10):
+    for j in range(5):
+        if i < 5 or i > 7:
+            if i < 5:
+                print("break")
+                break
+        print(f"{i}, {j}")
+    else:
+        # this block of code is executed if the inner loop completes without hitting a break statement
+        print("inner loop completed")
+
+    print("------------------")
+
+
+
+    def learn(self, X, y):
+
+        for features, label in np.ndier([X, y]):
             # found = False
 
             for law in self.laws:
@@ -51,7 +101,7 @@ class feedbackModel:
                 predication = self.default_label
                 explanation = self.default_explanation
 
-                print(f"predicted: {predication}, with the explanation of {explanation}")
+                print(f"DEFAULT: predicted: {predication}, with the explanation of {explanation}")
 
                 if predication != label:
                     # get discriminative feature from teacher
@@ -61,23 +111,6 @@ class feedbackModel:
                     new_law = Law(explanation, label, discriminative_feature)
                     self.laws.append(new_law)
                     pass
-
-
-"""
-
-for i in range(10):
-    for j in range(5):
-        if i < 5 or i > 7:
-            if i < 5:
-                print("break")
-                break
-        print(f"{i}, {j}")
-    else:
-        # this block of code is executed if the inner loop completes without hitting a break statement
-        print("inner loop completed")
-
-    print("------------------")
-
 
 
 """
