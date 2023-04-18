7bb2802817e1242e5810efe2a0296ee893a90be8
omerEtzion <etzion.omer@gmail.com>
2023-04-18 15:14:14 +0300

    teacher2

--- Teacher2.py
+++ Teacher2.py
@@ -14,4 +14,33 @@ class Teacher2(Teacher):
     """
 
     def teach(self, example, explanation, prediction):
-        pass
+        true_label = self.features_labels_dict[tuple(example)]
+        if true_label == prediction:
+            return true_label, None
+
+        # print(f"\ntrue label: {true_label}\nprediction: {prediction}")
+        # print(f"example: {example}\nexplanation: {explanation}\nexample != explanation: {example != explanation}\n")
+
+        chosen_discriminative_feature = self.most_discriminative_feature(example, explanation, prediction, true_label)
+        return true_label, chosen_discriminative_feature  # TODO return tuple?
+
+
+    def most_discriminative_feature(self, example, explanation, prediction, true_label):
+        different_indexes = np.where(example != explanation)[0]
+        max_difference = 0 
+        chosen_discriminative_feature = None
+        for i in different_indexes:
+            num_of_stsfyng_exmpls_with_prediction = len({k: v for k, v in self.features_labels_dict.items() if k[i] == example[i] and v == prediction})
+            num_of_stsfyng_exmpls_with_true_label = len({k: v for k, v in self.features_labels_dict.items() if k[i] == example[i] and v == true_label})
+            
+            prediction_percentage = num_of_stsfyng_exmpls_with_prediction/len(self.features_labels_dict)
+            true_label_percentage = num_of_stsfyng_exmpls_with_true_label/len(self.features_labels_dict)
+            difference = np.abs(prediction_percentage - true_label_percentage)
+            
+            if difference > max_difference:
+                max_difference = difference
+                chosen_discriminative_feature = [i, example[i]]
+                
+        return chosen_discriminative_feature
+            
+            
